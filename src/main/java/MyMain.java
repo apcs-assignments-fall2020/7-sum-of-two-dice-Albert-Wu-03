62f756f07dd8e15c3f23ca3f918a659d9d8cf8f3
Albert-Wu-03 <22qwu@athenian.org>
2020-09-26 13:57:42 +0000

    Sum of two dice

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,20 +1,26 @@
 import java.util.Scanner;
-
+import java.util.concurrent.ThreadLocalRandom;
 public class MyMain {
 
     // Rolls a single die
     // Returns a random integer between 1 and 6
     public static int rollDie() {
-        // REPLACE YOUR CODE HERE
-        return -1;
+        int n = ThreadLocalRandom.current().nextInt(1, 7);
+        return n;
     }
 
     // Rolls two dice n times
     // This method returns an int array of size 11, where each element is
     // the number of times a certain sum of two dice was rolled
     public static int[] sumOfTwoDice(int n) {
-        // REPLACE YOUR CODE HERE
-        return null;
+        int[] dice = new int[11];
+        for(int b = n; b > 0; b--) {
+		int d = ThreadLocalRandom.current().nextInt(1, 7);
+        int d1 = ThreadLocalRandom.current().nextInt(1, 7);
+        int i = d+d1-2;
+        dice[i]+=1; 
+	}
+    return dice;
     }
 
 
